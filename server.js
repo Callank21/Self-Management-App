b5f0d65796512a45a62b5f638649d937d79e479f
luisgcodes <lcienfuegos95@gmail.com>
2022-05-24 11:58:43 -0700

    installing tailwind

--- server.js
+++ server.js
@@ -8,13 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Have engine use handlebars template
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({defaultLayout: 'main'}));
 // Look for files that end with .handlebars
 app.set('view engine', 'handlebars');
 
 // Routes & Renders homepage.handlebars
 app.get('/', function (req, res) {
-    res.render('homepage');
+    res.render('homepage', {
+        title: 'Home Page',
+// can pass more values here(eg.): "name: 'lorem'"
+    });
+});
+
+// Routes & Renders dashboard.handlebars
+app.get('/dashboard', function (req, res) {
+    res.render('dashboard', {
+        title: 'My Dashboard'
+    });
 });
 // =============================================================
 
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./views/**/*.handlebars"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
