6261df64b0357b1e77d7b3b49bc4c6c1bae2c045
luisgcodes <lcienfuegos95@gmail.com>
2022-05-24 09:57:31 -0700

    express-handlebars running

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const {engine}  = require('express-handlebars');
+
+// Sets up the Express-Handlebars
+// =============================================================
 const app = express();
-const exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-
 // PORT
 const PORT = process.env.PORT || 3001;
 
-// Listener
+// Have engine use handlebars template
+app.engine('handlebars', engine());
+// Look for files that end with .handlebars
+app.set('view engine', 'handlebars');
+
+// Routes & Renders homepage.handlebars
+app.get('/', function (req, res) {
+    res.render('homepage');
+});
+// =============================================================
+
+
+// Lister
 app.listen(PORT, () => {
     console.log(`App listening on port http://localhost:${PORT}!`);
   });
