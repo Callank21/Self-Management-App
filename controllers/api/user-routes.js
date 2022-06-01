8e33d5a7e7c356b746af1350fad0f4abdda4a025
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-06-01 08:57:56 -0700

    commiting local changes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,21 +18,7 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_text', 'created_at']
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
-          model: Post,
-          attributes: ['title']
-        }
-      }
-    ]
+    }
   })
     .then(dbUserData => {
       if (!dbUserData) {
