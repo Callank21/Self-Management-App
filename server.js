46fca49b9b9fe380b1192cc512a5d986d66a9279
Seungjin <infinissible@gmail.com>
2022-05-31 20:50:18 -0700

    develop updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {engine}  = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Have engine use handlebars template
-app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 // Look for files that end with .handlebars
 app.set('view engine', 'handlebars');
 
@@ -22,32 +22,31 @@ app.use(routes);
 // Looks for style.css
 app.use(express.static('public'));
 
-
 // Routes & Renders homepage.handlebars
 app.get('/', function (req, res) {
-    res.render('homepage', {
-        title: 'Home Page',
-// can pass more values here(eg.): "name: 'lorem'"
-    });
+  res.render('homepage', {
+    title: 'Home Page',
+    // can pass more values here(eg.): "name: 'lorem'"
+  });
 });
 
 // Routes & Renders dashboard.handlebars
 app.get('/dashboard', function (req, res) {
-    res.render('dashboard', {
-        title: 'My Dashboard'
-    });
+  res.render('dashboard', {
+    title: 'My Dashboard',
+  });
 });
 
 app.get('/login', function (req, res) {
-    res.render('login', {
-        title: 'Log In'
-    });
+  res.render('login', {
+    title: 'Log In',
+  });
 });
 
 app.get('/signup', function (req, res) {
-    res.render('signup', {
-        title: 'Sign Up'
-    });
+  res.render('signup', {
+    title: 'Sign Up',
+  });
 });
 
 // // Routes & Renders dashboard.handlebars
@@ -58,10 +57,9 @@ app.get('/signup', function (req, res) {
 // });
 // =============================================================
 
-
 // Lister
-sequelize.sync({ force: false }).then(() =>{
-app.listen(PORT, () => {
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
     console.log(`App listening on port http://localhost:${PORT}!`);
   });
 });
