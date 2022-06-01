170d36b4a85a12b293310eefa1fff3d0f532b10c
Seungjin <infinissible@gmail.com>
2022-05-31 21:02:50 -0700

    move app.use(routes)

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
 // Looks for style.css
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes & Renders homepage.handlebars
 app.get('/', function (req, res) {
@@ -56,6 +55,8 @@ app.get('/signup', function (req, res) {
 //     });
 // });
 // =============================================================
+
+app.use(routes);
 
 // Lister
 sequelize.sync({ force: false }).then(() => {
