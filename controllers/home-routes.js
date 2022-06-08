7b382f6b2fa96c136bdcc4e97244871b153f87bc
Seungjin <infinissible@gmail.com>
2022-06-08 01:56:13 -0700

    display arrays in dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Project } = require('../models');
+const { Project, Heading, Task } = require('../models');
 
 router.get('/', function (req, res) {
   res.render('homepage', {
@@ -22,12 +22,6 @@ router.get('/signup', function (req, res) {
 });
 
 router.get('/dashboard', function (req, res) {
-  res.render('dashboard', {
-    title: 'My Dashboard',
-  });
-});
-
-router.get('/dashboard', (req, res) => {
   Project.findAll({
     attributes: ['id', 'project_title'],
     include: {
@@ -43,12 +37,42 @@ router.get('/dashboard', (req, res) => {
       const projects = dbCategoryData.map((project) =>
         project.get({ plain: true })
       );
-      res.render('dashboard', { projects });
+      console.log(projects[0].headings[0].tasks);
+      res.render(
+        'dashboard',
+        { projects }
+
+        // title: 'My Dashboard'
+      );
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
+// router.get('/dashboard', (req, res) => {
+// Project.findAll({
+//   attributes: ['id', 'project_title'],
+//   include: {
+//     model: Heading,
+//     attributes: ['id', 'heading_title', 'project_id'],
+//     include: {
+//       model: Task,
+//       attributes: ['id', 'desc', 'time', 'heading_id'],
+//     },
+//   },
+// })
+//     .then((dbCategoryData) => {
+//       const projects = dbCategoryData.map((project) =>
+//         project.get({ plain: true })
+//       );
+//       res.render('dashboard', { projects });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 module.exports = router;
