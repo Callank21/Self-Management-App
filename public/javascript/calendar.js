3c1ca4b06585eb3c7814fb0fd64d6881e6c9079a
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-06-09 22:22:44 -0700

    starting calendar

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -1,5 +1,22 @@
 const button = document.querySelector('#menu-button'); // Hamburger Icon
 const menu = document.querySelector('#menu'); // Menu
+import Calendar from 'tui-calendar'; /* ES6 */
+import "tui-calendar/dist/tui-calendar.css";
+
+// If you use the default popups, use this.
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
+
+var calendar = new Calendar('#calendar', {
+  defaultView: 'month',
+  taskView: true,
+  template: {
+    monthDayname: function(dayname) {
+      return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+    }
+    
+  }
+});
 
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
